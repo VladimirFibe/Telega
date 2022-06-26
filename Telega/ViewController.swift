fdf0298ee83cd7bff969e5bb184741cde7089e5d
Vladimir <motiw@icloud.com>
2022-06-27 00:23:14 +0600

    Initial Commit

---
+++ Telega/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Telega
+//
+//  Created by Vladimir Fibe on 27.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
